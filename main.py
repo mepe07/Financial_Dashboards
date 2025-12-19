b331849ee1e2c06afd27e41dc630a06190dbdbb2
AlexIPCA <168771740+AlexIPCA@users.noreply.github.com>
2025-12-19 22:44:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from src import ler_ficheiro_ps2, validar_dados
 from src import ler_ficheiro_ps2
 from src import converterParaPandas
 import sys
@@ -11,32 +10,30 @@ def main():
         # Chamar função em leitura_ps2.py
         lista_dados = ler_ficheiro_ps2(nome_ficheiro)
 
-        dados_validados = validar_dados(lista_dados)
-
         # Converter dados retornados para uma tabela com recurso ao Pandas
         df_Pandas = converterParaPandas(lista_dados)
         df_cabecalho = df_Pandas["cabecalho"]
         df_movimentos = df_Pandas["movimentos"]
         df_rodape = df_Pandas["rodape"]
 
-        #print(df_cabecalho)
-        #print()
-        #print(df_movimentos)
-        #print()
-        #print(df_rodape)
+        print(df_cabecalho)
+        print()
+        print(df_movimentos)
+        print()
+        print(df_rodape)
 
 
         # print("--- Tipos de Dados (Verificação) ---")
         # print(df_cabecalho.dtypes)
         
 
-        #Teste do leitura_ps2.py
-        #for registo in lista_dados:
-            #print("NOVO REGISTO")
-            #print("")
-            #for chave, valor in registo.items():
-                #print(f"{chave}: {valor}")
-                #print("")
+         #Teste do leitura_ps2.py
+        for registo in lista_dados:
+            print("NOVO REGISTO")
+            print("")
+            for chave, valor in registo.items():
+                print(f"{chave}: {valor}")
+                print("")
 
     except FileNotFoundError as e:
         print(f"Ficheiro não encontrado: {e}")
